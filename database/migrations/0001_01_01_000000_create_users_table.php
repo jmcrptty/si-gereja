c7da73dcaa6006b8af5a181938cd595c62d0b6ad
jmcrptty <joseph.corputty@gmail.com>
2025-07-12 00:03:13 +0900

    masukan data lingkungan

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->enum('role', ['pastor paroki', 'ketua lingkungan', 'sekretaris'])->default('ketua lingkungan');
-            $table->string('lingkungan')->nullable();
+            $table->string('lingkungan',100)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
